2f6b327692815c4a678c12b8a2f6067845b27537
Jonathan De Mangelaere <jonathan.de.mangelaere@student.howest.be>
2019-12-11 16:28:41 +0100

    Leeftijd wordt berkend adhv geboortedatum

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("reg-pw").addEventListener("input", checkPw);
     document.getElementById("reg-pw-second").addEventListener("input", checkPw);
+    document.getElementById("age").addEventListener("input", calculateDob);
 });
 
 function checkPw(){
@@ -12,7 +13,7 @@ function checkPw(){
     if(pwConditionCheck(pw) === true){
         pwRepeatCheck(pw, pwSecond);
     } else {
-        displayMessage("Zorg ervoor dat het wachtwoord minstens\n1 kleine letter\n1 hoofdletter\n1 cijfer bevat.");
+        displayMessage("pw-notice", "Zorg ervoor dat het wachtwoord minstens\n1 kleine letter\n1 hoofdletter\n1 cijfer bevat.");
     }
 }
 
@@ -26,12 +27,39 @@ function pwRepeatCheck(pw, pwSecond){
         displayMessage("");
     }
     if(pw === pwSecond){
-        displayMessage("Wachtwoorden komen overeen");
+        displayMessage("pw-notice", "Wachtwoorden komen overeen");
     } else {
-        displayMessage("Wachtwoorden komen niet overeen");
+        displayMessage("pw-notice", "Wachtwoorden komen niet overeen");
     }
 }
 
-function displayMessage(message){
-    document.getElementById("pw-notice").innerHTML = message;
+function displayMessage(elementId, message){
+    document.getElementById(elementId).innerHTML = "";
+    document.getElementById(elementId).innerHTML = message;
+}
+
+
+function calculateDob(){
+    let test = document.getElementById("age").value;
+    getAge(test);
+}
+
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    console.log(age);
+    showAgeMessage(age);
+}
+
+function showAgeMessage(age){
+    if (age >= 16){
+        displayMessage("age-notice", "U bent oud genoeg");
+    } else {
+        displayMessage("age-notice", "U bent te jong");
+    }
 }
