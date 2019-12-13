09cf659f1175354bb8e69ec776837aebedd90a19
Jonathan De Mangelaere <jonathan.de.mangelaere@student.howest.be>
2019-12-13 14:37:44 +0100

    Moved files

--- php/config.php
+++ php/config.php
@@ -1,14 +1,46 @@
 <?php
-define('DB_SERVER', 'localhost:3306');
-define('DB_USERNAME', 'jonathan');
-define('DB_PASSWORD', 'Melanie');
-define('DB_NAME', 'user');
- 
-/* Attempt to connect to MySQL database */
-$link = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("Error: kan geen verbinding maken" . mysql_connect_error());
+
+class Config
+{
+    private static $configInstantie = null;
+
+    private $server;
+    private $database;
+    private $username;
+    private $password;
+
+    private function __construct()
+    {
+        $this->server = "localhost";
+        $this->database = "dbHoneypot";
+        $this->username = "dev";
+        $this->password = "duikboottrampoline";
+    }
+
+    public static function getConfigInstantie()
+    {
+        if(is_null(self::$configInstantie))
+        {
+            self::$configInstantie = new Config();
+        }
+        return self::$configInstantie;
+    }
+
+    public function getServer()
+    {
+        return $this->server;
+    }
+    public function getDatabase()
+    {
+        return $this->database;
+    }
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
 }
-?>
